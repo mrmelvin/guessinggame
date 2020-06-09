35f908b9ff01a2a27f58116f8ed598d08224e5fb
mrmelvin <korobov.mike@gmail.com>
2020-06-09 19:59:20 +0500

    Added correct algorhytm

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-numbers_files=$(ls -l $PWD | wc -l)
+numbers_files=$(ls -l $PWD | tail -n+2 | wc -l)
 echo "Please, type number of files in the current directory"
 read users_numbers_files
-echo "files: $numbers_files"
 if [[ $numbers_files -eq $users_numbers_files ]]
 then 
 	echo "Correct"
