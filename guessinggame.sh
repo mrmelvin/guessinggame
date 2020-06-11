c7832ddd8bd4b563c0e26a08c7f1666dce2b8634
mrmelvin <korobov.mike@gmail.com>
2020-06-11 19:50:11 +0500

    fix logic

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 numbers_files=$(ls -l $PWD | tail -n+2 | wc -l)
 echo "Please, type number of files in the current directory"
+
+while [[ $numbers_files -ne $users_numbers_files ]]
+do
 read users_numbers_files
-if [[ $numbers_files -eq $users_numbers_files ]]
-then 
-	echo "Correct"
-elif [[ $numbers_files -lt $users_numbers_files ]]
-then
-	echo "More than real numbers of files"
-elif [[ $numbers_files -gt $users_numbers_files ]]
-then
-        echo "Less than real numbers of files"
-fi
+	if [[ $users_numbers_files =~ [[:alpha:]] ]]
+	then
+        echo "Please, type some digit"
+	elif [[ $numbers_files -eq $users_numbers_files ]]
+	then
+			echo "Congratulations you guessed right!"
+	elif [[ $numbers_files -lt $users_numbers_files ]]
+	then
+			echo "More than real numbers of files"
+	elif [[ $numbers_files -gt $users_numbers_files ]]
+	then
+			echo "Less than real numbers of files"
+	fi
+
+done
+
+
+
+
+
+
+
